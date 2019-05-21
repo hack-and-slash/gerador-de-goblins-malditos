07766832dcb01e91e00b0db70320508dc8062a44
degrecci <hdegrecci@gmail.com>
2019-05-20 21:08:03 -0300

    Altera fonte e utiliza wired-card

--- src/App.js
+++ src/App.js
@@ -18,19 +18,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Goblin</h1>
-      <p>{nome}</p>
-      <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
-      <p>{`Ocupação: ${ocupacao}`}</p>
+      <wired-card>
+        <h1>Goblin</h1>
+        <p>{nome}</p>
+        <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
+        <p>{`Ocupação: ${ocupacao}`}</p>
 
-      <h3>Atributos</h3>
-      <p>{`Combate: ${combate}`}</p>
-      <p>{`Conhecimento: ${conhecimento}`}</p>
-      <p>{`Habilidade: ${habilidade}`}</p>
-      <p>{`Sorte: ${sorte}`}</p>
+        <h3>Atributos</h3>
+        <p>{`Combate: ${combate}`}</p>
+        <p>{`Conhecimento: ${conhecimento}`}</p>
+        <p>{`Habilidade: ${habilidade}`}</p>
+        <p>{`Sorte: ${sorte}`}</p>
 
-      <h3>Equipamentos</h3>
-      <p>{equipamento}</p>
+        <h3>Equipamentos</h3>
+        <p>{equipamento}</p>
+      </wired-card>
     </div>
   );
 }
