044a68a93774756fd9f91f8e59c9dd020c9e1829
degrecci <hdegrecci@gmail.com>
2019-05-20 21:13:41 -0300

    Updates

---
+++ precache-manifest.0ed1d049193ca20a5c1120ff6ac16bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb9bd1b0a63ff2c1ab0cd4e55d5d8b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8acc3eebbf74a697abd",
+    "url": "/static/css/main.d3dd104e.chunk.css"
+  },
+  {
+    "revision": "6462a21e0b2a6cbb0907",
+    "url": "/static/js/2.78d8d388.chunk.js"
+  },
+  {
+    "revision": "e8acc3eebbf74a697abd",
+    "url": "/static/js/main.840112cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
