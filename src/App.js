45faee4fb3d8af227b9128c1d9960db5761a1679
degrecci <hdegrecci@gmail.com>
2019-06-11 21:23:35 -0300

    Remove dependencia wired-elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { WiredCard } from 'wired-elements';
 import goblinMaldito from './gerador';
 
 function App() {
@@ -18,21 +17,19 @@ function App() {
 
   return (
     <div className="App">
-      <wired-card>
-        <h1>Goblin</h1>
-        <p>{nome}</p>
-        <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
-        <p>{`Ocupação: ${ocupacao}`}</p>
+      <h1>Goblin</h1>
+      <p>{nome}</p>
+      <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
+      <p>{`Ocupação: ${ocupacao}`}</p>
 
-        <h3>Atributos</h3>
-        <p>{`Combate: ${combate}`}</p>
-        <p>{`Conhecimento: ${conhecimento}`}</p>
-        <p>{`Habilidade: ${habilidade}`}</p>
-        <p>{`Sorte: ${sorte}`}</p>
+      <h3>Atributos</h3>
+      <p>{`Combate: ${combate}`}</p>
+      <p>{`Conhecimento: ${conhecimento}`}</p>
+      <p>{`Habilidade: ${habilidade}`}</p>
+      <p>{`Sorte: ${sorte}`}</p>
 
-        <h3>Equipamentos</h3>
-        <p>{equipamento}</p>
-      </wired-card>
+      <h3>Equipamentos</h3>
+      <p>{equipamento}</p>
     </div>
   );
 }
