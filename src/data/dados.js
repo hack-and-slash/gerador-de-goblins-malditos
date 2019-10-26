d3b4a377a6a67b4fdbc91905667a6d1d5a6cf94c
Renato66 <revi66@hotmail.com>
2019-10-26 17:39:06 -0300

    refactoring dicerolls  0-5 and 1-6

---
+++ src/data/dados.js
@@ -0,0 +1,18 @@
+import { Random } from 'random-js';
+
+const rolarD6 = () => {
+  const random = new Random();
+
+  return random.integer(0, 5);
+};
+
+const rolarDadoD6 = () => {
+  const random = new Random();
+
+  return random.integer(1, 6);
+};
+
+export {
+  rolarD6,
+  rolarDadoD6
+}
