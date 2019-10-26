cad3c9f75d78f77048d1bea7259554b953019b4f
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-26 13:05:53 -0300

    importado componente goblin e estilos globais

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
 import goblinMaldito from './gerador';
+import GlobalStyle from './styles/global';
+import { Goblin } from './styles/styles';
 
 function App() {
   const {
@@ -16,21 +17,24 @@ function App() {
   } = goblinMaldito();
 
   return (
-    <div className="App">
-      <h1>Goblin</h1>
-      <h2>{nome}</h2>
-      <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
-      <p>{`Ocupação: ${ocupacao}`}</p>
+    <>
+      <Goblin>
+        <h1>Goblin</h1>
+        <h2>{nome}</h2>
+        <p>{`Aparência: ${coloracao} e ${caracteristica}`}</p>
+        <p>{`Ocupação: ${ocupacao}`}</p>
 
-      <h3>Atributos</h3>
-      <p>{`Combate: ${combate}`}</p>
-      <p>{`Conhecimento: ${conhecimento}`}</p>
-      <p>{`Habilidade: ${habilidade}`}</p>
-      <p>{`Sorte: ${sorte}`}</p>
+        <h3>Atributos</h3>
+        <p>{`Combate: ${combate}`}</p>
+        <p>{`Conhecimento: ${conhecimento}`}</p>
+        <p>{`Habilidade: ${habilidade}`}</p>
+        <p>{`Sorte: ${sorte}`}</p>
 
-      <h3>Equipamentos</h3>
-      <p>{equipamento}</p>
-    </div>
+        <h3>Equipamentos</h3>
+        <p>{equipamento}</p>
+      </Goblin>
+      <GlobalStyle />
+    </>
   );
 }
 
