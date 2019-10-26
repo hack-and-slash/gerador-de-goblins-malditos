aa6818645f9f4d5e00f837afc6ab266c4bef6b00
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-26 13:05:31 -0300

    criado componente goblin

---
+++ src/styles/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Goblin = styled.div`
+  text-align: center;
+
+  h1 {
+    font-size: 40px;
+    color: var(--primaryColor);
+  }
+`;
