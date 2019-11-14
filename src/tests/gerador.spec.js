84447e27d1be8cfdb2a7ddd65ce2c1728e22dbb6
edumoreira1506 <contato@eduardoem.com.br>
2019-11-14 10:10:56 -0300

    Testes para gerarCaracteristica

--- src/tests/gerador.spec.js
+++ src/tests/gerador.spec.js
@@ -2,6 +2,7 @@ import gerarNomeDoGoblin from '../data/nomes';
 import gerarEquipamento from '../data/equipamentos';
 import tabelaGoblin from '../data/tabelaGoblin';
 import tabelaDeEquipamentos from '../data/tabelaDeEquipamentos';
+import gerarCaracteristica from '../data/caracteristicas';
 
 describe('gerador', () => {
   describe('gerarNomeDoGoblin', () => {
@@ -63,6 +64,41 @@ describe('gerador', () => {
       const ocupacao = 'Xamã';
       const equipamento = gerarEquipamento(ocupacao);
       expect(equipamento).toBe('Cajado');
+    });
+  });
+
+  describe('gerarCaracteristica', () => {
+    const opcoesValidas = [1, 2, 3, 4, 5];
+    const opcoesInvalidas = [7, 8, 9, 10];
+
+    opcoesValidas.forEach((opcao) => {
+      describe(`quando o número é ${opcao}`, () => {
+        it('retorna uma string', () => {
+          expect(typeof gerarCaracteristica(opcao)).toBe('string');
+        });
+
+        it('retorna uma string válida', () => {
+          expect(Boolean(gerarCaracteristica(opcao))).toBeTruthy();
+        });
+      });
+    });
+
+    opcoesInvalidas.forEach((opcao) => {
+      describe(`quando o número é ${opcao}`, () => {
+        it('retorna uma string', () => {
+          expect(typeof gerarCaracteristica(opcao)).toBe('string');
+        });
+
+        it('retorna uma string inválida', () => {
+          expect(Boolean(gerarCaracteristica(opcao))).toBeFalsy();
+        });
+      });
+    });
+
+    describe('quando o número é 6', () => {
+      it('retorna um array', () => {
+        expect(typeof gerarCaracteristica(6)).toBe('object');
+      });
     });
   });
 });
