3a70c6cae994618d57d49d14e4067be5b021eef9
degrecci <hdegrecci@gmail.com>
2021-01-25 17:10:22 -0300

    gerador.js -> gerador.ts

--- src/gerador.ts
+++ src/gerador.ts
@@ -4,8 +4,9 @@ import tabelaGoblin from './data/tabelaGoblin';
 import tabelaDeAtributos from './data/tabelaDeAtributos';
 import gerarNomeDoGoblin from './data/nomes';
 import gerarEquipamento from './data/equipamentos';
+import { Atributos, Coloracao, Ocupacao } from './types';
 
-const calcularAtributo = (coloracao, ocupacao, atributo) => {
+const calcularAtributo = (coloracao: Coloracao, ocupacao: Ocupacao, atributo: Atributos) => {
   const atributoPorColoracao = tabelaDeAtributos.coloracao[coloracao][atributo];
   const atributoPorOcupacao = tabelaDeAtributos.ocupacao[ocupacao][atributo];
 
@@ -13,9 +14,9 @@ const calcularAtributo = (coloracao, ocupacao, atributo) => {
 };
 
 const goblinMaldito = () => {
-  const coloracao = tabelaGoblin.coloracao[rolarD6()];
+  const coloracao = tabelaGoblin.coloracao[rolarD6()] as Coloracao;
   const caracteristica = gerarCaracteristica(rolarDadoD6());
-  const ocupacao = tabelaGoblin.ocupacao[rolarD6()];
+  const ocupacao = tabelaGoblin.ocupacao[rolarD6()] as Ocupacao;
   const nome = gerarNomeDoGoblin();
   const combate = calcularAtributo(coloracao, ocupacao, 'combate');
   const conhecimento = calcularAtributo(coloracao, ocupacao, 'conhecimento');
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Atributos = 'sorte' | 'conhecimento' | 'habilidade' | 'combate';
+
+export type Coloracao = 'Verde Claro' | 'Verde Escuro' | 'Vermelho' | 'Verde' | 'Amarelo' | 'Azul';
+
+export type Ocupacao = 'Mercenário' | 'Caçador' | 'Gatuno' | 'Líder' | 'Piromaníaco' | 'Xamã';
